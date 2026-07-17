67cdf67562844528640030effe9357e21c33b439
pvsxxnga-max <pvsxxnga@gmail.com>
2026-07-17 16:44:11 +0700

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,1 +1,18 @@
-
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Sarabun', 'sans-serif'],
+      },
+      colors: {
+        primary: '#1E40AF', // โทนสีน้ำเงินกรมท่า e-LIS
+        secondary: '#E0E7FF',
+        success: '#10B981',
+        warning: '#F59E0B',
+        danger: '#EF4444',
+      }
+    },
+  },
+  plugins: [],
+}
